4159c725490516e0712ff2a7666e9696ec0619db
Andrii Vasyliev <sol@hiqdev.com>
2016-10-27 13:19:01 +0000

    added ServiceInterface

---
+++ src/ServiceInterface.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * Library for confirmation tokens
+ *
+ * @link      https://github.com/hiqdev/php-confirmator
+ * @package   php-confirmator
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\confirmator;
+
+interface ServiceInterface
+{
+    public function issueToken(array $data);
+    public function checkToken($token, array $data);
+    public function findToken($token);
+    public function removeToken($token);
+}
