79189b3c436c005951eb13e3d11d89b0a94c1c2c
Andrey Klochok <andreyklochok@gmail.com>
2018-08-27 18:29:00 +0300

    Returned original FileStorage, modified yii2/FileStorage

---
+++ src/FileStorage.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * Library for confirmation tokens.
+ *
+ * @link      https://github.com/hiqdev/php-confirmator
+ * @package   php-confirmator
+ * @license   BSD-3-Clause
+ * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
+ */
+
+namespace hiqdev\php\confirmator;
+
+class FileStorage implements StorageInterface
+{
+    public $path;
+
+    public function __construct($path = null)
+    {
+        $this->path = $path;
+    }
+
+    protected function getFullPath($name)
+    {
+        return $this->path . '/' . $name[0] . '/' . $name;
+    }
+
+    public function has($name)
+    {
+        return is_file($this->getFullPath($name));
+    }
+
+    public function get($name)
+    {
+        $path = $this->getFullPath($name);
+
+        return is_file($path) ? file_get_contents($this->getFullPath($name)) : null;
+    }
+
+    public function set($name, $text)
+    {
+        $path = $this->getFullPath($name);
+        $dir = dirname($path);
+        if (!is_dir($dir)) {
+            mkdir($dir, 0755, true);
+        }
+
+        return file_put_contents($path, $text);
+    }
+
+    public function remove($name)
+    {
+        $path = $this->getFullPath($name);
+
+        return unlink($path);
+    }
+}
--- yii2/FileStorage.php
+++ yii2/FileStorage.php
@@ -1,59 +1,13 @@
 <?php
-/**
- * Library for confirmation tokens.
- *
- * @link      https://github.com/hiqdev/php-confirmator
- * @package   php-confirmator
- * @license   BSD-3-Clause
- * @copyright Copyright (c) 2016-2017, HiQDev (http://hiqdev.com/)
- */
 
 namespace hiqdev\yii2\confirmator;
 
-use hiqdev\php\confirmator\StorageInterface;
 use Yii;
 
-class FileStorage implements StorageInterface
+class FileStorage extends \hiqdev\php\confirmator\FileStorage
 {
-    public $path;
-
     public function __construct($path = null)
     {
-        $this->path = Yii::getAlias($path);
-    }
-
-    protected function getFullPath($name)
-    {
-        return $this->path . '/' . $name[0] . '/' . $name;
-    }
-
-    public function has($name)
-    {
-        return is_file($this->getFullPath($name));
-    }
-
-    public function get($name)
-    {
-        $path = $this->getFullPath($name);
-
-        return is_file($path) ? file_get_contents($this->getFullPath($name)) : null;
-    }
-
-    public function set($name, $text)
-    {
-        $path = $this->getFullPath($name);
-        $dir = dirname($path);
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-
-        return file_put_contents($path, $text);
-    }
-
-    public function remove($name)
-    {
-        $path = $this->getFullPath($name);
-
-        return unlink($path);
+        parent::__construct(Yii::getAlias($path));
     }
 }
